659e22be21bd5775ef91c4f9c4cc81a8e817b008
Kalman Tarnay <kalmi@users.noreply.github.com>
2013-02-05 18:07:16 +0100

    debug off

--- app.py
+++ app.py
@@ -20,7 +20,6 @@ import sendmail
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.debug = True
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 
